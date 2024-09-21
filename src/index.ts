d7752573da2ab1faf3ebda39d7c7f1ee86b1a9b5
dr1co <dricomatoba@hotmail.com>
2024-09-21 14:11:53 -0300

    [feat.] add all routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
 import fastify from "fastify";
+import z from "zod";
 
 import { env } from "./env/env";
+import { createGoal } from "./functions/createGoal";
+import { getWeekPendingGoals } from "./functions/getWeekPendingGoals";
+import { createGoalCompletion } from "./functions/createGoalCompletion";
 
 const server = fastify();
+
+server.get("/pending-goals", async () => {
+  const { pendingGoals } = await getWeekPendingGoals();
+
+  return pendingGoals;
+});
+
+server.post("/goals", async (req) => {
+  const CreateRegisterSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  });
+
+  const { title, desiredWeeklyFrequency } = CreateRegisterSchema.parse(
+    req.body
+  );
+
+  await createGoal({
+    title,
+    desiredWeeklyFrequency,
+  });
+});
+
+server.post("/completions", async (req) => {
+  const CreateCompletionSchema = z.object({
+    goalId: z.string().cuid2(),
+  });
+
+  const { goalId } = CreateCompletionSchema.parse(req.body);
+
+  await createGoalCompletion({
+    goalId,
+  });
+});
 
 server
   .listen({
